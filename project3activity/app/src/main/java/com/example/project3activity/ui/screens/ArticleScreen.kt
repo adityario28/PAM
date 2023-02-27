6e48c9b348942a42e919020795a363994de32fe3
Mahesa Rio Aditya <Adityario28@gmail.com>
2023-02-27 16:20:15 +0700

    V1.7.4
    Article Page, Profile Page, more string

--- project3activity/app/src/main/java/com/example/project3activity/ui/screens/ArticleScreen.kt
+++ project3activity/app/src/main/java/com/example/project3activity/ui/screens/ArticleScreen.kt
@@ -1,36 +1,123 @@
 package com.example.project3activity.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.project3activity.R
+import com.example.project3activity.ui.theme.Project3activityTheme
+import com.example.project3activity.ui.theme.Shapes
 
 @Composable
 fun ArticleScreen(){
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(colorResource(id = R.color.purple_700))
-            .wrapContentSize(Alignment.Center)
+//            .background(colorResource(id = R.color.purple_700))
+//            .wrapContentSize(Alignment.Center)
     ) {
-        Text(
-            text = "Profile Screen",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
-        )
+        Column(
+            modifier = Modifier
+                .padding(top = 20.dp)
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally)
+        ) {
+                Image(painter = painterResource(id = R.drawable.hero_news), contentDescription = "Article")
+        }
+
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp)
+        ) {
+            Text(
+                text = stringResource(id = R.string.Headline), style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                fontSize = 22.sp
+            ))
+        }
+        
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, end = 16.dp, top = 4.dp)) {
+            Row {
+                Icon(painter = painterResource(id = R.drawable.person), contentDescription = "Author", modifier = Modifier.size(16.dp))
+
+                Text(text = stringResource(id = R.string.author),
+                    modifier = Modifier
+                        .padding(start = 2.dp)
+                    ,style = TextStyle(
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal
+                ))
+
+                Icon(painter = painterResource(id = R.drawable.date), contentDescription = "Date", modifier = Modifier
+                    .size(20.dp)
+                    .padding(start = 8.dp))
+                
+                Text(text = stringResource(id = R.string.date),
+                modifier = Modifier
+                    .padding(start = 2.dp),
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal
+                )
+                )
+                
+            }
+
+        }
+
+        Column(modifier = Modifier
+            .padding(start = 16.dp, end = 16.dp, top = 12.dp)) {
+            Text(text = stringResource(id = R.string.news_text), style = TextStyle(
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Normal,
+                textAlign = TextAlign.Justify,
+                lineHeight = 24.sp
+            )
+            )
+
+        }
+
+
+
+//        Text(
+//            text = "Profile Screen",
+//            fontWeight = FontWeight.Bold,
+//            color = Color.White,
+//            modifier = Modifier.align(Alignment.CenterHorizontally),
+//            textAlign = TextAlign.Center,
+//            fontSize = 20.sp
+//        )
 
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewArticle() {
+    Project3activityTheme {
+//        Greeting2("Android")
+        ArticleScreen()
+    }
+
 }
--- project3activity/app/src/main/java/com/example/project3activity/ui/screens/ProfileScreen.kt
+++ project3activity/app/src/main/java/com/example/project3activity/ui/screens/ProfileScreen.kt
@@ -1,38 +1,191 @@
 package com.example.project3activity.ui.screens
 
 import android.graphics.Paint.Align
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowForward
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.project3activity.R
+import com.example.project3activity.ui.theme.Project3activityTheme
 
 @Composable
 fun ProfileScreen(){
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(colorResource(id = R.color.purple_700))
-            .wrapContentSize(Alignment.Center)
+//            .background(colorResource(id = R.color.purple_700))
+//            .wrapContentSize(Alignment.Center)
     ) {
-        Text(
-            text = "Profile Screen",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
-        )
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, top = 48.dp)
+            ) {
+                Card(border = BorderStroke(width = 2.dp, color = colorResource(id = R.color.bg_splash)), shape = RoundedCornerShape(40.dp)) {
+                    Image(painter = painterResource(id = R.drawable.ava_square), contentDescription = "Avatar", modifier = Modifier.height(60.dp))
+
+                }
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Column(modifier = Modifier
+                    .align(Alignment.CenterVertically)) {
+                    Text(text = "test",
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                    )
+
+                    Text(text = "test_sub",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Thin
+                    ))
+
+                }
+
+            }
+
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Divider(modifier = Modifier.height(1.dp), color = Color.Gray)
+
+            Row(modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, top = 12.dp, bottom = 12.dp)
+                .fillMaxWidth())
+            {
+                Image(painter = painterResource(id = R.drawable.doctor_icon_1), contentDescription = "doctor profile", modifier = Modifier.size(28.dp))
+                
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(start = 8.dp)
+                        .weight(1f),
+                    text = "Recent Doctor",
+                    style = TextStyle(
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 16.sp,
+                        color = Color.Black
+                    ))
+
+                Icon(imageVector = Icons.Rounded.ArrowForward, contentDescription = "Arrow Right", modifier = Modifier.align(Alignment.CenterVertically) )
+            }
+
+            Divider(modifier = Modifier.height(1.dp), color = Color.Gray)
+
+            Row(modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, top = 12.dp, bottom = 12.dp)
+                .fillMaxWidth())
+            {
+                Image(painter = painterResource(id = R.drawable.doctor_icon_1), contentDescription = "doctor profile", modifier = Modifier.size(28.dp))
+
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(start = 8.dp)
+                        .weight(1f),
+                    text = "Recent Doctor",
+                    style = TextStyle(
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 16.sp,
+                        color = Color.Black
+                    ))
+
+                Icon(imageVector = Icons.Rounded.ArrowForward, contentDescription = "Arrow Right", modifier = Modifier.align(Alignment.CenterVertically) )
+            }
+
+            Divider(modifier = Modifier.height(1.dp), color = Color.Gray)
+
+            Row(modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, top = 12.dp, bottom = 12.dp)
+                .fillMaxWidth())
+            {
+                Image(painter = painterResource(id = R.drawable.doctor_icon_1), contentDescription = "doctor profile", modifier = Modifier.size(28.dp))
+
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(start = 8.dp)
+                        .weight(1f),
+                    text = "Recent Doctor",
+                    style = TextStyle(
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 16.sp,
+                        color = Color.Black
+                    ))
+
+                Icon(imageVector = Icons.Rounded.ArrowForward, contentDescription = "Arrow Right", modifier = Modifier.align(Alignment.CenterVertically) )
+            }
+
+            Divider(modifier = Modifier.height(1.dp), color = Color.Gray)
+
+            Row(modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp, top = 12.dp, bottom = 12.dp)
+                .fillMaxWidth())
+            {
+                Image(painter = painterResource(id = R.drawable.doctor_icon_1), contentDescription = "doctor profile", modifier = Modifier.size(28.dp))
+
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(start = 8.dp)
+                        .weight(1f),
+                    text = "Recent Doctor",
+                    style = TextStyle(
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 16.sp,
+                        color = Color.Black
+                    ))
+
+                Icon(imageVector = Icons.Rounded.ArrowForward, contentDescription = "Arrow Right", modifier = Modifier.align(Alignment.CenterVertically) )
+            }
+
+            Divider(modifier = Modifier.height(1.dp), color = Color.Gray)
+
+
+        }
+//        Text(
+//            text = "Profile Screen",
+//            fontWeight = FontWeight.Bold,
+//            color = Color.White,
+//            modifier = Modifier.align(Alignment.CenterHorizontally),
+//            textAlign = TextAlign.Center,
+//            fontSize = 20.sp
+//        )
 
     }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewProfile() {
+    Project3activityTheme {
+//        Greeting2("Android")
+        ProfileScreen()
+    }
+
 }
