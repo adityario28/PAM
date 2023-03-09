d149702f9e4e91be5ef41c69375a1bbe1cc3eda3
Mahesa Rio Aditya <Adityario28@gmail.com>
2023-03-09 14:42:00 +0700

    V1.9.1

--- project5webservice/app/src/main/java/com/example/project5webservice/MainActivity.kt
+++ project5webservice/app/src/main/java/com/example/project5webservice/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
