a7066570de3908931a5b4b1a06049e0a624a2252
Mahesa Rio Aditya <Adityario28@gmail.com>
2023-03-14 15:30:38 +0700

    fix

--- project6firebase/app/src/main/java/com/example/project6firebase/MainActivity.kt
+++ project6firebase/app/src/main/java/com/example/project6firebase/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.project6firebase
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.view.textclassifier.TextClassifierEvent.ConversationActionsEvent
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -14,11 +13,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-//import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asAndroidBitmap
@@ -28,18 +24,17 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.project6firebase.ui.theme.Project6firebaseTheme
-import com.example.project6firebase.R
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import java.io.ByteArrayOutputStream
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Date
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Project6firebaseTheme {
+            Project6firebaseTheme() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -50,24 +45,18 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
-    private fun uploadImage(img:ImageBitmap) {
+    private fun uploadImage(img: ImageBitmap){
         val fStorage = Firebase.storage
         val storageRef = fStorage.reference
 
         val fileName = SimpleDateFormat("yyyMMddHHmm'.png'").format(Date())
         val ref = storageRef.child("images/$fileName")
 
-//      Convert Bitmap to ByteArray
         val stream = ByteArrayOutputStream()
         img.asAndroidBitmap().compress(Bitmap.CompressFormat.PNG, 100, stream)
         val image = stream.toByteArray()
 
-        //Upload process
         var uploadTask = ref.putBytes(image)
-
-//        val ref = storageRef.child("images/mountains.jpg")
-//        uploadTask = ref.putFile(file)
 
         val urlTask = uploadTask.continueWithTask { task ->
             if (!task.isSuccessful) {
@@ -85,54 +74,55 @@ class MainActivity : ComponentActivity() {
                 // ...
             }
         }
-
     }
-
-
 }
 
 @Composable
-fun MainScreen( onSubmitActionEvent: (img: ImageBitmap) -> Unit) {
-    var lContext = LocalContext.current
+fun MainScreen (
+    onSubmitActionEvent: (img: ImageBitmap) -> Unit
+){
 
-    var captionText by remember {
-        mutableStateOf("")
-    }
-
+    val lContext = LocalContext.current
+    var captionText by remember { mutableStateOf("") }
     var takenImage by remember {
-        mutableStateOf(BitmapFactory.decodeResource(lContext.resources, R.drawable.no_image).asImageBitmap())
+        mutableStateOf(
+            BitmapFactory.decodeResource(lContext.resources, R.drawable.no_image).asImageBitmap()
+        )
     }
-
 
     val takePictureContract = rememberLauncherForActivityResult(
-        contract =  ActivityResultContracts.TakePicturePreview(),
-        onResult = {_takenImageBitmap ->
+        contract = ActivityResultContracts.TakePicturePreview(),
+        onResult = { _takenImageBitmap ->
             takenImage = _takenImageBitmap.asImageBitmap()
-        })
+        }
+    )
 
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Row() {
-            Image(bitmap = takenImage, contentDescription = "no image", modifier = Modifier
-                .size(120.dp)
-                .clickable { takePictureContract.launch() })
-
-            Column() {
-                TextField(value = "", onValueChange = {captionText = it})
-
-                Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth()) {
+    Column(modifier = Modifier.padding(4.dp)) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Image(bitmap = takenImage, contentDescription = "",
+                modifier = Modifier
+                    .size(120.dp)
+                    .padding(end = 4.dp)
+                    .clickable {
+                        takePictureContract.launch()
+                    })
+            Column(modifier = Modifier.fillMaxWidth()) {
+                TextField(value = captionText, onValueChange = {})
+                Button(onClick = { onSubmitActionEvent(takenImage) }) {
                     Text(text = "Submit")
                 }
             }
-
         }
-
     }
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun MainPreview() {
+
+//@Preview(showBackground = true)
+//@Composable
+//fun MainScreenPreview(){
 //    MainScreen()
-}
+//}
